51a33a9b6c36a4bc023edb5333665dba59893445
Jones Daniel da Silva <jonesd_1@hotmail.com>
2019-07-03 08:22:17 -0300

    Subida projeto inicial

---
+++ src/ListWishes.Domain/Users/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ListWishes.Domain.Users
+{
+    public class User : Entity<User>
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+    }
+}
